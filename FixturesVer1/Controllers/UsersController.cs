d91190ceb7d7d229e0ecfa35220711ded6592014
M-Muneeb <sheikhmohd.muneeb@gmail.com>
2015-01-31 03:13:43 -0800

    Commit Profile & Slider

--- FixturesVer1/Controllers/UsersController.cs
+++ FixturesVer1/Controllers/UsersController.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FixturesVer1.Models;
+using FixturesVer1.Services;
 
 namespace FixturesVer1.Controllers
 {
 
     public class UsersController : Controller
     {
+        [HttpGet]
+        [AllowAnonymous]
         new public ActionResult Profile()
         {
+           
+            return View();
+           
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        new public ActionResult Profile(User userModel)
+        {
+            //string encryptedPass = Utility.Cryptography.Encryption(userModel.usr_Password);
+            //userModel.usr_Password = encryptedPass;
+            return View();
+
+        }
+         public ActionResult DashBoard()
+        {
+
             return View();
         }
+         
 	}
 }
