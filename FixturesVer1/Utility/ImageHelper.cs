5841dfa0519324f4fce56c322d6d07728fc6e391
M-Muneeb <sheikhmohd.muneeb@gmail.com>
2015-02-14 15:39:05 -0800

    Complete Profile

--- FixturesVer1/Utility/ImageHelper.cs
+++ FixturesVer1/Utility/ImageHelper.cs
@@ -2,21 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-        using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+
 using System.Web.Mvc;
 namespace FixturesVer1.Utility
 {
     public static class ImageHelper
     {
-        public static MvcHtmlString Image(this HtmlHelper helper, string src, string altText, string height)
+        public static MvcHtmlString Image(this HtmlHelper helper, string src, string altText, string height, string width)
         {
             var builder = new TagBuilder("img");
             builder.MergeAttribute("src", src);
             builder.MergeAttribute("alt", altText);
             builder.MergeAttribute("height", height);
+            builder.MergeAttribute("width", width);
             return MvcHtmlString.Create(builder.ToString(TagRenderMode.SelfClosing));
         }
     }
